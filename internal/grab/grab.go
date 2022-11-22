029c05d9142584be1a28cd7e48444a346b5cf9c3
easbarba <easbarba@outlook.com>
2022-11-22 13:03:26 -0300

    grab: import git-go

--- internal/grab/grab.go
+++ internal/grab/grab.go
@@ -1,7 +1,24 @@
 package grab
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-git/go-git/v5"
+)
+
+// TODO: After grabbing log informations
 
 func Grab() {
 	fmt.Println("grabbing")
+
+	_, err := git.PlainClone("/tmp/go-git", false, &git.CloneOptions{
+		URL:      "https://github.com/go-git/go-git",
+		Progress: os.Stdout,
+	})
+	CheckIfError(err)
+}
+
+func CheckIfError(err error) {
+	fmt.Println(err)
 }
