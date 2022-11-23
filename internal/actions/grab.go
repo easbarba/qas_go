f51b8925b96bc57e92a3d643bd5bdf9e8c864324
easbarba <easbarba@outlook.com>
2022-11-22 23:14:37 -0300

    actions/grab: implement both pull and clone

--- internal/actions/grab.go
+++ internal/actions/grab.go
@@ -21,7 +21,13 @@ func Grab(dest string, projects []config.Config) {
 			fld := path.Join(dest, project.Lang, p.Name)
 
 			printInfo(fld, p.Name, p.Url, p.Branch)
-			// clone(fld, p.Name, p.Url, p.Branch)
+
+			if _, err := os.Stat(path.Join(fld, ".git")); err == nil {
+				pull(fld, p.Url)
+			} else {
+				clone(fld, p.Name, p.Url, p.Branch)
+			}
+
 		}
 	}
 }
@@ -37,6 +43,9 @@ folder: %s
 }
 
 func clone(folder, name, url, branch string) {
+	fmt.Println("status: cloning")
+	fmt.Println("")
+
 	_, err := git.PlainClone(folder, false, &git.CloneOptions{
 		URL:      url,
 		Progress: os.Stdout,
@@ -45,6 +54,25 @@ func clone(folder, name, url, branch string) {
 	CheckIfError(err)
 }
 
+func pull(folder, url string) {
+	fmt.Println("status: pulling")
+	fmt.Println("")
+
+	o, err := git.PlainOpen(folder)
+	CheckIfError(err)
+
+	w, err := o.Worktree()
+	CheckIfError(err)
+
+	w.Pull(&git.PullOptions{RemoteName: "origin"})
+	CheckIfError(err)
+}
+
 func CheckIfError(err error) {
-	fmt.Println(err)
+	if err == nil {
+		return
+	}
+
+	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
+	os.Exit(1)
 }
