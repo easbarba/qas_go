137f2a22544db63a2f9a203cec2f5f9322faddc4
easbarba <easbarba@outlook.com>
2023-01-03 21:03:31 -0300

    main: simple flag parsing

--- cmd/qas/main.go
+++ cmd/qas/main.go
@@ -8,36 +8,28 @@ import (
 	"github.com/easbarba/qas/internal/actions"
 )
 
+var verbose = flag.Bool("verbose", false, "display more information")
+var grab = flag.Bool("grab", false, "grab floss projects")
+var archive = flag.String("archive", "", "archive floss projects listed on NAMES")
+
 func main() {
-	grab, archive, verbose := cliParse()
-
-	if *grab == true {
-		actions.Grab(verbose)
-	}
-
-	if *archive != "" {
-		actions.Archive(archive, verbose)
-	}
-}
-
-// command line arguments parser
-func cliParse() (*bool, *string, *bool) {
-	verbose := flag.Bool("verbose", false, "display more information")
-	grab := flag.Bool("grab", false, "grab floss projects")
-	archive := flag.String("archive", "", "archive floss projects listed on NAMES")
-
-	flag.Parse()
-
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "qas - Easily manage multiple FLOSS repositories. \n")
 		fmt.Fprintln(flag.CommandLine.Output(), "\nUsage information:")
 		flag.PrintDefaults()
 	}
 
-	if *grab == false && *archive == "" {
+	flag.Parse()
+
+	if !*grab && *archive == "" {
 		flag.Usage()
 		os.Exit(0)
 	}
 
-	return grab, archive, verbose
+	if *grab {
+		actions.Grab(verbose)
+		return
+	}
+
+	actions.Archive(archive, verbose)
 }
